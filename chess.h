a4b07afd36be97180ea5f3c389067ee95442f0f3
ssdfx007008 <gcui3@illinois.edu>
2016-10-21 23:15:40 -0500

    chess make

--- chess.h
+++ chess.h
@@ -19,6 +19,7 @@ struct state {
 		int ** maze= new int*[16];
 		for(int y=0;y<16;y++)
 		{
+			maze[y]=new int[16];
 			for(int x=0;x<16;x++)
 			{
 				// if maze[y][x]=0, then black, else white, 2 means nothing
@@ -35,23 +36,31 @@ struct state {
 
 		}
 	}
+	~state(){
+		for(int y=0;y<16;y++)
+		{
+			delete maze[y];	
+		}
+		delet maze;
+	}
 	int value;
+	std::vector<state> next_states;
 };
 class chess
 {
 public:
 	chess(){
 		state first_state;
-		states.push_back(first_state);
+		current_state=first_state;
 	};
 
 	~chess();
-	std::vector<state> states;	
-	void initialize();
+	state current_state;
 	void tree_construction();
 
 	// return 0 means game not finished, else finished
-	int make_decision();
-
+	int make_decision(state cur);
+private:
+	void construct(int depth,state);
 };
 
