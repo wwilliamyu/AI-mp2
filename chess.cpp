331f625c8dd506d653bd632454ef1e3dd71cfe3b
ssdfx007008 <gcui3@illinois.edu>
2016-10-23 13:46:10 -0500

    fasd

--- chess.cpp
+++ chess.cpp
@@ -123,7 +123,6 @@ void chess::create_state(state * curr, int prev_y, int prev_x, int new_y, int ne
 	temp->board[new_y][new_x] = player; // white piece to left diag
 	curr->next_states.push_back(temp);
 	print_board(temp);
-	delete temp;
 }
 
 void chess::make_decision(state* &current,state* next){
